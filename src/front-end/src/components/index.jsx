dbbf6fd992defc0d9e31a1e01df410d70b5e1f65
Andrew Golightly <support@andrewgolightly.com>
2016-12-22 00:20:34 +0700

    made screen clickable to change question

--- src/front-end/src/components/index.jsx
+++ src/front-end/src/components/index.jsx
@@ -9,26 +9,43 @@ class DeepQuestionsApp extends Component {
 
     this.state = {
       questions: [],
+      currentQuestion: '',
     };
+
+    this.changeQuestion = this.changeQuestion.bind(this);
   }
 
   componentDidMount() {
-    fetch('http://golightlyplus.com:6900/questions?num=9')
+    fetch('http://golightlyplus.com:6900/questions?num=3')
     .then(response => response.json())
     .then((questions) => {
-      console.log(questions);
-      this.setState({ questions });
+      this.setState({ questions, currentQuestion: questions[0] });
     }).catch((ex) => {
       console.log('parsing failed', ex);
     });
   }
 
-  render() {
-    const question = this.state.questions[0];
+  changeQuestion() {
+    const allQuestions = this.state.questions;
+    const randomIndex = Math.floor(Math.random() * allQuestions.length);
+    const randomQ = allQuestions[randomIndex];
+    allQuestions.splice(randomIndex, 1);
+    this.setState({
+      questions: allQuestions,
+      currentQuestion: randomQ,
+    });
+  }
 
+  render() {
+    let content;
+    if (this.state.questions.length === 0) {
+      content = 'Game over';
+    } else {
+      content = this.state.currentQuestion;
+    }
     return (
-      <div className={styling['deep-questions-app']}>
-        {question}
+      <div className={styling['deep-questions-app']} onClick={this.changeQuestion}>
+        {content}
       </div>
     );
   }
